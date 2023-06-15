c71ba501754544936d923f091f355d39c3c393a9
Dharvik19 <dharvik.badga22@gmail.com>
2023-06-15 19:58:32 +0530

    making the mails realtime

--- src/Components/Pages/Inbox.js
+++ src/Components/Pages/Inbox.js
@@ -1,15 +1,39 @@
-import React from "react"; 
+import React, { useEffect, useState } from "react"; 
 import EmailList from './EmailList';
 import { useDispatch,useSelector } from 'react-redux';
+import { Container } from "react-bootstrap";
 const Inbox = ()=>{
     const mails = useSelector((state)=>state.mail.mails);
+    const emailId = localStorage.getItem('email');
+const regex = /[.@]/g;
+const email = emailId.replace(regex, '');
+    const [data, setData] =useState([])
+    useEffect(()=>{
+        async  function  fetchData(){
+              const response = await fetch(`https://mailboxclient-7e135-default-rtdb.firebaseio.com/mails/${email}.json`);
+              if(!response.ok){
+                  throw new Error("cant fetch")
+              }
+      
+              const data = await response.json();
+              // data.filter((item)=> item.id === id)
+              console.log(data.mails) ;            
+              setData(data.mails);
+          }   
+         let interval =  setInterval(() => {
+            fetchData();
+          }, 2000);
+          return () =>{
+            clearInterval(interval);
+          }
+      },[])
     return (
-        <>
+        <Container>
             <h2>
                 Inbox
             </h2>
             {/* need to create a seperate array for inbox in firebase and fetch it  */}
-            {mails?.map((item) => (
+            {data?.map((item) => (
           <EmailList
             key={item.id}
             item={{
@@ -20,7 +44,7 @@ const Inbox = ()=>{
             }}
           />
         ))}
-        </>
+        </Container>
     )
 }
 
